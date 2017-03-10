3d7ed4316e5545ba6b8eac5286c319186c1593a9
Thiago Heitling <thiago.heitling@gmail.com>
2017-03-10 10:54:20 -0800

    - Start iOS project. - Set colour to the tab bar item when it is in ‘selected’ state

---
+++ TreatmentApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TreatmentApp
+//
+//  Created by Thiago Heitling on 2017-03-10.
+//  Copyright © 2017 Thiago Heitling. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TreatmentApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TreatmentApp
+//
+//  Created by Thiago Heitling on 2017-03-10.
+//  Copyright © 2017 Thiago Heitling. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
