4e7a496be7364ccbf68e986b445847d539546386
Thiago Heitling <thiago.heitling@gmail.com>
2017-03-11 17:42:24 -0800

    - Body Joints View completed with single selection of each body joint - Add custom tab bar icons

--- TreatmentApp/BodyViewController.swift
+++ TreatmentApp/BodyViewController.swift
@@ -9,17 +9,53 @@
 import UIKit
 
 class BodyViewController: UIViewController {
-
+    
+    // All joints (buttons) are referenced by a tag number in storyboard.
+    // They are sequenced in pairs(right side < left side) from bottom part of the body to the upper part.
+    // So tag 1 = right foot , tag 2 = left foot, and so on.
+    
+    let jointTags = 19
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.title = "BODY JOINTS"
+        
+        hideButtons()
+    }
+    
+    func hideButtons() {
+        
+        for tag in 0...jointTags {
+        
+            let joint = self.view.viewWithTag(tag) as? UIButton
+            joint?.setBackgroundImage(nil, for: .normal)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func jointPressed(_ sender: UIButton) {
+        
+        toggleJointImages(with: sender)
     }
-
-
+    
+    func toggleJointImages(with joint: UIButton) {
+        
+        if joint.currentBackgroundImage == nil {
+            joint.setBackgroundImage(UIImage(named:"JointButton"), for: .normal)
+        }
+        else {
+            joint.setBackgroundImage(nil, for: .normal)
+        }
+        
+        for tag in 0...jointTags {
+            
+            if tag != joint.tag {
+                
+                let untouchedJoint = self.view.viewWithTag(tag) as? UIButton
+                untouchedJoint?.setBackgroundImage(nil, for: .normal)
+            }
+        }
+    }
+    
 }
 
